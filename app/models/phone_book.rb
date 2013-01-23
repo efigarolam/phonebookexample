cd591d47c549c54c6751693e6dca89c88cd2d105
Eduardo <eduardo.figarola@crowdint.com>
2013-01-23 10:00:49 -0600

    Add age to attr_accessor

--- app/models/phone_book.rb
+++ app/models/phone_book.rb
@@ -1,5 +1,5 @@
 class PhoneBook < ActiveRecord::Base
-  attr_accessible :address, :last_name, :name, :phone
+  attr_accessible :address, :last_name, :name, :phone, :age
   validates :name, presence: true
   validates :phone, presence: true
   validates :age, numericality: true, allow_blank: true
